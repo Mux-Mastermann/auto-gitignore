4e55e22a41e63e61a761a6f8f2a3aa390733f6fc
Mux-Mastermann <43514294+Mux-Mastermann@users.noreply.github.com>
2020-05-11 21:44:11 +0200

    Created setup file for dist

--- gitignore.py
+++ gitignore.py
@@ -11,31 +11,35 @@ logging.basicConfig(level=logging.WARNING,
                     format="%(asctime)s — %(levelname)s — Line:%(lineno)d — %(message)s")
 
 
-# request to get all possible templates
-r = requests.get("https://www.gitignore.io/api/list?format=lines")
-templates = [line for line in r.text.splitlines()]
-logging.debug(templates)
+def main():
+    """
+    Using the https://www.gitignore.io/ API to get templates for a gitignore file
+    """
+    # request to get all possible templates
+    r = requests.get("https://www.gitignore.io/api/list?format=lines")
+    templates = [line for line in r.text.splitlines()]
+    logging.debug(templates)
 
-# setting the input completer to the list of templates from the API
-input_completer = WordCompleter(templates)
-# prompting the user for template input and setting them as parameter for next API call
-parameter = prompt("Enter all templates with a space in between: ",
-                   completer=input_completer, complete_while_typing=True).split()
-logging.info(parameter)
-# Test if all parameters are in the templates list (taking care of invalid input here)
-for param in parameter:
-    if param not in templates:
-        print(f"Invalid Input: {param} not a valid template")
-        quit()
+    # setting the input completer to the list of templates from the API
+    input_completer = WordCompleter(templates)
+    # prompting the user for template input and setting them as parameter for next API call
+    parameter = prompt("Enter all templates with a space in between: ",
+                       completer=input_completer, complete_while_typing=True).split()
+    logging.info(parameter)
+    # Test if all parameters are in the templates list (taking care of invalid input here)
+    for param in parameter:
+        if param not in templates:
+            print(f"Invalid Input: {param} not a valid template")
+            quit()
 
-# actual request with parameters
-r = requests.get(f"https://www.gitignore.io/api/{','.join(parameter)}")
-logging.debug(f"Request: {r.request.path_url}")
-logging.info(r.status_code)
+    # actual request with parameters
+    r = requests.get(f"https://www.gitignore.io/api/{','.join(parameter)}")
+    logging.debug(f"Request: {r.request.path_url}")
+    logging.info(r.status_code)
 
-# write returned text into .gitignore file in the dir from which script was run
-with open(".gitignore", "w") as wf:
-    wf.write(r.text)
+    # write returned text into .gitignore file in the dir from which script was run
+    with open(".gitignore", "w") as f:
+        f.write(r.text)
 
-# Success
-print("Successfully created your gitignore file")
+    # Success
+    print("Successfully created your gitignore file")
---
+++ setup.py
@@ -0,0 +1,20 @@
+"""gitignore Setup
+
+Setup specifications for gitignore project.
+"""
+from setuptools import setup
+
+setup(
+    name="gitignore",
+    version="0.1",
+    py_modules=["gitignore"],
+    entry_points={
+        "console_scripts": [
+            "gitignore = gitignore:main"
+        ]
+    },
+    install_requires=[
+        "requests",
+        "prompt-toolkit"
+    ]
+)
