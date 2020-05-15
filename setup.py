73ae645fdbf0cc38ffe4501a86e372b04a013af6
Mux-Mastermann <43514294+Mux-Mastermann@users.noreply.github.com>
2020-05-15 23:23:12 +0200

    Completed setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
-"""gitignore Setup
+"""Setup specifications for gitignore project."""
 
-Setup specifications for gitignore project.
-"""
+from os import path
+
 from setuptools import setup
 
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
-    name="gitignore",
-    version="0.2",
+    name="auto-gitignore",
+    version="1.0.0",
+    description="Create gitignore template with autocompletion.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Mux-Mastermann/gitignore",
+    author="Jan Knorr",
+    author_email="jan.hhc+dev@gmail.com",
     py_modules=["gitignore"],
     entry_points={
         "console_scripts": [
